38d5704bb4f1b7a9a802b610ac9bcb5830c53337
Kimia Hosseini <kimia.hosseini1381@gmail.com>
2023-07-18 11:31:12 +0330

    generate some bugs:)

--- Phase1/src/ReadPrinciple.java
+++ Phase1/src/ReadPrinciple.java
@@ -17,12 +17,13 @@ public class ReadPrinciple {
     public boolean splitBy(Character c){
         if(splitMarks.isEmpty())
             return isAlphabetic(c);
-        return splitMarks.contains(c.toString());
+        return !splitMarks.contains(c.toString());
     }
     private boolean isAlphabetic(char c) {
         return c >= 'a' && c <= 'z';
     }
     public void setSplitMarks(String splitMarks) {
         this.splitMarks = new ArrayList<>(Arrays.asList(splitMarks.split("")));
+        System.out.println(splitMarks);
     }
 }
