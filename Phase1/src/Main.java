439659d9046eeb3466303661f8e6506efd5bf632
Kiasoh <kiarashs37@gmail.com>
2023-07-19 09:32:53 +0330

    final final commit

--- Phase1/src/Main.java
+++ Phase1/src/Main.java
@@ -1,17 +1,22 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        FileReaderClass.setPath("./books/");
-//        FileReaderClass.addValidFormat(".txt");
+        FileReaderClass.setPath("./books/");
+        FileReaderClass.addValidFormat(".txt");
 //        Scanner in = new Scanner(System.in);
 //        System.out.println("\u001B[36mwrite the query then split marks\nExample: get help +illness +disease -cough /sm  ,# !.\nto quit type :q");
 //        ReadPrinciple readPrinciple = new ReadPrinciple();
-//        System.out.println(Execute.run(in, readPrinciple));
+//        printResult(Execute.run(in, readPrinciple));
         ReadPrinciple readPrinciple = new ReadPrinciple();
         readPrinciple.setSplitMarks(" ");
         readPrinciple.normalization = new Stemmer();
-        System.out.print(Execute.run("kiarash" , readPrinciple).toString().replaceAll(",","\n"));;
+        printResult(Execute.run("learn +kiarash +small -dog" , readPrinciple));
+    }
+
+    private static void printResult(Set<String> res){
+        System.out.println(res.toString().replaceAll(",","\n"));
     }
 }
