64b5c55ec1f5106b40741344ff54a3b5947ee8c5
Kimia Hosseini <kimia.hosseini1381@gmail.com>
2023-07-26 17:05:11 +0330

    QueryListsTest completed

--- Phase1/src/test/java/QueryListsTest.java
+++ Phase1/src/test/java/QueryListsTest.java
@@ -1,10 +1,39 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class QueryListsTest {
-    
-    ReadPrinciple readPrinciple = new ReadPrinciple();
+
+    @Mock
+    ReadPrinciple readPrinciple;
     QueryLists queryLists = new QueryLists();
+    String[] query = new String[]{"word1","+word2","-word3","word4"};
 
-    public void categorizationTest(){
-        String query = "word1 +word2 -word3";
+    @BeforeEach
+    public void setup(){
+        readPrinciple = mock(ReadPrinciple.class);
+        when(readPrinciple.getNormalization()).thenReturn(new NullNormalization());
+        queryLists.categorization(query,readPrinciple);
+    }
 
+    @Test
+    public void categorizationEssentialTest(){
+        assertTrue(queryLists.getEssential().contains("word1"));
+        assertTrue(queryLists.getEssential().contains("word4"));
+    }
+
+    @Test
+    public void categorizationOptionalTest(){
+        assertTrue(queryLists.getOptional().contains("word2"));
+    }
+
+    @Test
+    public void categorizationForbiddenTest(){
+        assertTrue(queryLists.getForbidden().contains("word3"));
     }
 }
