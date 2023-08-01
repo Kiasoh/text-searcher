0497f5d909de08036b50ada37d8f936852f9ee35
Kimia Hosseini <kimia.hosseini1381@gmail.com>
2023-08-01 14:48:48 +0330

    style

--- phase6/Messenger/src/main/java/Validations.java
+++ phase6/Messenger/src/main/java/Validations.java
@@ -2,6 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validations {
+
     public static String PhoneNumberValidation(String phoneNumber) throws Exception {
         Pattern pattern = Pattern.compile("^09[0-9]{9}");
         Matcher matcher = pattern.matcher(phoneNumber);
@@ -9,6 +10,7 @@ public class Validations {
             throw new Exception("PhoneNumber ain't it brov!");
         return phoneNumber;
     }
+
     public static String passwordValidation(String pass) throws Exception {
         Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,32}$");
         Matcher matcher = pattern.matcher(pass);
@@ -16,5 +18,4 @@ public class Validations {
             throw new Exception("password ain't it brov!");
         return pass;
     }
-
 }
