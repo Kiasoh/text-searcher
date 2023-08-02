9f1b21bd6a899b3de87443526b4e4762a451c585
Kiasoh <kiarashs37@gmail.com>
2023-08-02 17:58:53 +0330

    addChat added (with no tests)

--- phase6/Messenger/src/main/java/Chat.java
+++ phase6/Messenger/src/main/java/Chat.java
@@ -1,8 +1,20 @@
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Entity
+@Setter
+@Getter
 @Table(name="Chat",
         uniqueConstraints={@UniqueConstraint(columnNames={"ChatID"})})
 public class Chat
@@ -10,13 +22,35 @@ public class Chat
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="ChatID", nullable=false, unique=true)
-    public int ChatID;
-    public String Title;
-    public java.sql.Timestamp CreatedAt;
+    @Setter(AccessLevel.NONE)
+    private int ChatID;
+    private String Title;
+    private java.sql.Timestamp CreatedAt;
 
     @OneToOne
     @JoinColumn(name = "ProfilePhotoID", referencedColumnName = "FileID")
-    public File ProfilePhotoID;
+    private File ProfilePhotoID;
 
-    public String Type;
+    private String Type;
+
+    public static int addChat (Session session , String title, String profilePath, String type) throws SQLException, IOException {
+        Chat chat = new Chat();
+        chat.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+        chat.setProfilePhotoID(File.addFile(profilePath));
+        chat.setTitle(title);
+        chat.setType(type);
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.persist(chat);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw e;
+        }
+        return chat.getChatID();
+    }
+    public static void joinChat (Session session, int chatID , String username, boolean isAdmin) {
+
+    }
 }
